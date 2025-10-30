b2ea75ad982eb39b8fbbfcebfb0de3cd65de7626
Luanzitobs <luan.bentto2017@gmail.com>
2025-10-30 11:41:18 -0300

    ContarEposição.py

---
+++ ContarEposição.py
@@ -0,0 +1,9 @@
+frutas = ["Goiaba",  "Goiaba" , "Caju", "Pera"]
+
+print (frutas.count("Goiaba"))
+print (frutas.count("Caju"))
+print (frutas.count("Pera"))
+print (frutas.index("Caju"))
+
+#.count serve para contar
+#.index serve para pegar a posição do elemento
