70a1c3385eda48a7e5e35583f3a48757db358e09
Luanzitobs <luan.bentto2017@gmail.com>
2025-10-31 16:53:42 -0300

    beecrowd1175.py

---
+++ beecrowd1175.py
@@ -0,0 +1,12 @@
+lista = []
+
+for i in range(20):
+    lista.append(int(input()))
+    
+x = lista [::-1]
+
+for j in range (20):
+    
+    print (f"N[{j}] = {x[j]}")
+    
+# (::-1 significa vim do fim para om inicio)
