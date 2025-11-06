3ede0d9a4b20e4bf055e9071de7402e540f672ae
Luanzitobs <luan.bentto2017@gmail.com>
2025-11-06 11:52:54 -0300

    Dicionario.py

---
+++ Dicionario.py
@@ -0,0 +1,5 @@
+pessoa = {"nome": input("Digite seu nome :"), "idade": int(input("Digite sua idade : ")), "CPF": int(input("Digite seu CPF :"))}
+
+print (f"Nome = {pessoa["nome"]}")
+print (f"Idade = {pessoa["idade"]}")
+print (f"CPF = {pessoa["CPF"]}")
