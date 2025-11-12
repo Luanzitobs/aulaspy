ce11d86d343d153fe47140201f8a9f4d8a5d56bd
Luanzitobs <luan.bentto2017@gmail.com>
2025-11-12 16:28:25 -0300

    beecrowd1944.py

---
+++ beecrowd1944.py
@@ -0,0 +1,20 @@
+participantes = int(input())
+lista = []
+brindes = 0
+
+for i in range (participantes):
+
+    if len(lista) == 0 :
+        lista.append("F A C E")
+
+    letras = input()
+
+    letrasInver = lista[-1][::-1] 
+
+    if letras == letrasInver:
+        brindes +=1
+        lista.pop()
+    else:
+        lista.append(letras)
+
+print (brindes)
